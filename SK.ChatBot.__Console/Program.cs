ec857fde743816a3c744ff4466bee5419a8c3022
Stephen <semmsdorf@globalsoftwarecorp.com>
2020-10-03 20:30:04 -0500

    Added Weather and Telegram skeleton libraries.

---
+++ SK.ChatBot.__Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SK.ChatBot.__Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
